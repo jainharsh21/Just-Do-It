b96793ea307c27233170d19aaca56ca614df01db
HARSH JAIN <jainharsh270@gmail.com>
2020-06-03 12:26:30 +0530

    initialised state and added app bar

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import Paper from "@material-ui/core/Paper";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
 
 function TodoApp() {
+  const initialTodos = [
+    { id: 1, task: "Work", completed: false },
+    { id: 2, task: "Sleep", completed: true },
+    { id: 3, task: "Eat", completed: false },
+  ];
+  const [todos, setTodos] = useState(initialTodos);
   return (
-    <div>
-      <h1>Todos</h1>
-    </div>
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: "100vh",
+        backgroundColor: "#fafafa",
+      }}
+    >
+      <AppBar color="primary" position="static" style={{ height: "64px" }}>
+        <Toolbar>
+          <Typography color="inherit">Just Do It</Typography>
+        </Toolbar>
+      </AppBar>
+    </Paper>
   );
 }
 export default TodoApp;
