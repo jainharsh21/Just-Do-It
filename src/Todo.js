f2edc242cc6721107ba54105362f77e1275b3ceb
HARSH JAIN <jainharsh270@gmail.com>
2020-06-04 12:07:14 +0530

    added checkbox,delete and edit buttons

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,31 @@
 import React from "react";
-import { ListItem, ListItemText } from "@material-ui/core";
+import {
+  ListItem,
+  ListItemText,
+  Checkbox,
+  ListItemSecondaryAction,
+  IconButton,
+} from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
 
 function Todo({ task, completed }) {
   return (
     <ListItem>
-      <ListItemText>{task}</ListItemText>
+      <Checkbox tabIndex={-1} checked={completed} />
+      <ListItemText
+        style={{ textDecoration: completed ? "line-through" : "none" }}
+      >
+        {task}
+      </ListItemText>
+      <ListItemSecondaryAction>
+        <IconButton aria-label="Delete">
+          <DeleteIcon />
+        </IconButton>
+        <IconButton aria-label="Edit">
+          <EditIcon />
+        </IconButton>
+      </ListItemSecondaryAction>
     </ListItem>
   );
 }
