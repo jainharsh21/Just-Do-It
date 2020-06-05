81ebfa219afeb746a68e2d9b2d8b994204bb0d8b
HARSH JAIN <jainharsh270@gmail.com>
2020-06-05 12:05:39 +0530

    completed edit todo functionality

--- src/Todo.js
+++ src/Todo.js
@@ -11,12 +11,12 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 import EditTodoForm from "./EditTodoForm";
 
-function Todo({ task, completed, id, removeTodo, toggleTodo }) {
+function Todo({ task, completed, id, removeTodo, toggleTodo, editTodo }) {
   const [isEditing, toggle] = useToggleState(false);
   return (
     <ListItem>
       {isEditing ? (
-        <EditTodoForm />
+        <EditTodoForm editTodo={editTodo} id={id} task={task} toggleEditForm={toggle} />
       ) : (
         <>
           <Checkbox
--- src/TodoApp.js
+++ src/TodoApp.js
@@ -26,6 +26,12 @@ function TodoApp() {
     );
     setTodos(updatedTodos);
   };
+  const editTodo = (todoId, newTask) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === todoId ? { ...todo, task: newTask } : todo
+    );
+    setTodos(updatedTodos);
+  };
   return (
     <Paper
       style={{
@@ -47,6 +53,7 @@ function TodoApp() {
             todos={todos}
             removeTodo={removeTodo}
             toggleTodo={toggleTodo}
+            editTodo={editTodo}
           />
         </Grid>
       </Grid>
