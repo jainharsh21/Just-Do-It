409be72021b93a310bf7f993f4d78a0810f53dfc
HARSH JAIN <jainharsh270@gmail.com>
2020-06-05 11:54:21 +0530

    added edit todo form component

---
+++ src/EditTodoForm.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { TextField } from "@material-ui/core";
+
+function EditTodoForm() {
+  return <TextField />;
+}
+
+export default EditTodoForm;
--- src/Todo.js
+++ src/Todo.js
@@ -9,13 +9,14 @@ import {
 } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
+import EditTodoForm from "./EditTodoForm";
 
 function Todo({ task, completed, id, removeTodo, toggleTodo }) {
   const [isEditing, toggle] = useToggleState(false);
   return (
     <ListItem>
       {isEditing ? (
-        <h1>Editing time</h1>
+        <EditTodoForm />
       ) : (
         <>
           <Checkbox
