10f7609d7472bc26da11cd715b4c36a8f5584774
HARSH JAIN <jainharsh270@gmail.com>
2020-06-14 11:14:45 +0530

    added reducer to whole app

--- src/EditTodoForm.js
+++ src/EditTodoForm.js
@@ -1,17 +1,17 @@
 import React, { useContext } from "react";
 import useInputState from "./hooks/useInputState";
 import TextField from "@material-ui/core/TextField";
-import { TodosContext } from "./contexts/todos.context";  
+import { TodosContext } from "./contexts/todos.context";
 
 function EditTodoForm({ id, task, toggleEditForm }) {
-  const { editTodo } = useContext(TodosContext);
+  const { dispatch } = useContext(TodosContext);
   const [value, handleChange, reset] = useInputState(task);
   console.log("EDIT FORM RE-RENDER");
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        editTodo(id, value);
+        dispatch({ type: "EDIT", id: id, newTask: value });
         reset();
         toggleEditForm();
       }}
